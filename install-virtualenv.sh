6dcd3d44acec16baa71fd3164aba24dd3a8dde7a
joelei <thezero12@hotmail.com>
2022-02-11 11:44:02 +0800

    Add: gvm and pyenv use opt path

--- install-virtualenv.sh
+++ install-virtualenv.sh
@@ -5,6 +5,7 @@ set -ex
 
 # vscode 依赖
 export GO111MODULE=on
+export GOPATH=/opt/go
 go get github.com/uudashr/gopkgs/v2/cmd/gopkgs@latest
 go get github.com/ramya-rao-a/go-outline@latest
 go get github.com/cweill/gotests/gotests@latest
@@ -17,24 +18,29 @@ go get honnef.co/go/tools/cmd/staticcheck@latest
 go get golang.org/x/tools/gopls@latest
 
 # gvm
-bash< <(curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer)
-source "/root/.gvm/scripts/gvm"
+export GVM_ROOT=/opt/gvm
+curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer -o /usr/local/bin/gvm-installer
+chmod a+x /usr/local/bin/gvm-installer && gvm-installer master /opt
+source "/opt/gvm/scripts/gvm"
 gvm install go1.15.15 -B
 gvm install go1.17.6 -B
 
 # python 环境
 pip install virtualenvwrapper supervisor flake8 black isort
 
-git clone https://github.com/pyenv/pyenv.git ~/.pyenv
-git clone https://github.com/pyenv/pyenv-virtualenv.git ~/.pyenv/plugins/pyenv-virtualenv
-git clone https://github.com/pyenv/pyenv-virtualenvwrapper.git ~/.pyenv/plugins/pyenv-virtualenvwrapper
-export PATH=/data/bin:/root/.pyenv/bin:/root/.pyenv/shims:$PATH
+export PYENV_ROOT=/opt/pyenv
+git clone https://github.com/pyenv/pyenv.git ${PYENV_ROOT}
+git clone https://github.com/pyenv/pyenv-virtualenv.git ${PYENV_ROOT}/plugins/pyenv-virtualenv
+git clone https://github.com/pyenv/pyenv-virtualenvwrapper.git ${PYENV_ROOT}/plugins/pyenv-virtualenvwrapper
+export PATH=/data/bin:${PYENV_ROOT}/bin:${PYENV_ROOT}/shims:$PATH
 pyenv install 2.7.18
 pyenv install 3.6.15
 pyenv install 3.10.1
 
 # 添加环境变量
-echo 'export PATH=/data/bin:/root/go/bin:/root/.pyenv/bin:/root/.pyenv/shims:$PATH' >> /root/.bashrc
+echo 'export PYENV_ROOT=/opt/pyenv' >> /root/.bashrc
+echo 'export GVM_ROOT=/opt/gvm' >> /root/.bashrc
+echo 'export PATH=/data/bin:/opt/go/bin:/opt/pyenv/bin:/opt/pyenv/shims:$PATH' >> /root/.bashrc
 echo 'exec zsh' >> /root/.bashrc
 
 # 解决 docker 内 zsh 不能自动补全问题
