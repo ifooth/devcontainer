d8808df75d36284eadf1245a59a8bc0d0923326f
ifooth <thezero12@hotmail.com>
2022-12-28 22:43:16 +0800

    Update: use golangci-lint for staticcheck

--- install-virtualenv.sh
+++ install-virtualenv.sh
@@ -37,14 +37,13 @@ cd /opt/gvm && tar -zcf pkgsets.tar.gz pkgsets
 # https://github.com/golang/vscode-go/blob/master/docs/tools.md
 gvm use $GO1_18
 export GOPATH=/opt/go
-go install github.com/uudashr/gopkgs/v2/cmd/gopkgs@latest
 go install github.com/ramya-rao-a/go-outline@latest
 go install github.com/cweill/gotests/gotests@latest
 go install github.com/fatih/gomodifytags@latest
 go install github.com/josharian/impl@latest
 go install github.com/haya14busa/goplay/cmd/goplay@latest
 go install github.com/go-delve/delve/cmd/dlv@latest
-go install honnef.co/go/tools/cmd/staticcheck@latest
+go install github.com/golangci/golangci-lint/cmd/golangci-lint@latest
 go install golang.org/x/tools/gopls@latest
 
 # golang小工具
