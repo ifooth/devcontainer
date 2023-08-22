3bb265a7b808a10c0dfa33b92c77429f72f397c5
joelei <thezero12@hotmail.com>
2023-08-22 22:43:06 +0800

    Update: refine dev-init.sh

--- dev-init.sh
+++ dev-init.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
-# devcontainer init script example
+# devcontainer init script
 
-git config --global user.name ""
-git config --global user.email ""
+if [ -n $GIT_USER_NAME ]
+    git config --global user.name $GIT_USER_NAME
+fi
 
-git config --global user.signingkey ""
+if [ -n $GIT_USER_EMAIL ]
+    git config --global user.email $GIT_USER_NAME
+fi
+
+if [ -n $GIT_USER_SIGNINGKEY ]
+    git config --global user.signingkey $GIT_USER_SIGNINGKEY
+    git config --global commit.gpgsign true
+fi
+
 git config --global credential.helper store
-git config --global commit.gpgsign true
 git config --global pull.ff only
