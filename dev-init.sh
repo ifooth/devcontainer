26d6e19dd2402e7ab7f653370a2c6d66311bbac8
ifooth <ifooth>
2023-08-23 00:47:19 +0800

    Update: fix dev-init.sh

--- dev-init.sh
+++ dev-init.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 # devcontainer init script
 
-if [ -n $GIT_USER_NAME ];then
+if [ -n "$GIT_USER_NAME" ];then
     git config --global user.name $GIT_USER_NAME
 fi
 
-if [ -n $GIT_USER_EMAIL ];then
-    git config --global user.email $GIT_USER_NAME
+if [ -n "$GIT_USER_EMAIL" ];then
+    git config --global user.email $GIT_USER_EMAIL
 fi
 
-if [ -n $GIT_USER_SIGNINGKEY ];then
+if [ -n "$GIT_USER_SIGNINGKEY" ];then
     git config --global user.signingkey $GIT_USER_SIGNINGKEY
     git config --global commit.gpgsign true
+else
+    git config --global --unset user.signingkey
+    git config --global commit.gpgsign false
 fi
 
 git config --global credential.helper store
