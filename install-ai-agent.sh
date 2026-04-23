5f6ec53c5cf3e7cae365c064afb90fa590ced5c1
ifooth <thezero12@hotmail.com>
2026-04-23 19:21:52 +0800

    remove install ai agent

--- install-ai-agent.sh
+++ install-ai-agent.sh
@@ -2,16 +2,20 @@
 set -ex
 
 # openspec
-echo "prefix=/opt/npm" > /opt/node/lib/node_modules/npm/npmrc
-npm install -g @fission-ai/openspec@latest
+# echo "prefix=/opt/npm" > /opt/node/lib/node_modules/npm/npmrc
+# npm install -g @fission-ai/openspec@latest
 
 # cursor cli
-export HOME=/opt/cursor
-curl https://cursor.com/install -fsS | bash
+# export HOME=/opt/cursor
+# curl https://cursor.com/install -fsS | bash
 
 mkdir -p /opt/bin
-cp -rf /opt/cursor/.local/bin/agent /opt/npm/bin
-cp -rf /opt/cursor/.local/bin/cursor-agent /opt/npm/bin
+# cp -rf /opt/cursor/.local/bin/agent /opt/npm/bin
+# cp -rf /opt/cursor/.local/bin/cursor-agent /opt/npm/bin
+
+# opencode
+# OPENCODE_VERSION="v1.14.21"
+# curl -fsSL "https://github.com/anomalyco/opencode/releases/download/${OPENCODE_VERSION}/opencode-linux-x64.tar.gz" | tar -xz -C /opt/bin/
 
 # 全局配置
 echo "prefix=/root/.npm-packages" > /opt/node/lib/node_modules/npm/npmrc
